c1fc24e468435bc9ea9e4efd16a753dad688c1d8
srinivasa-reddy-07 <srinivasareddykapa18@gmail.com>
2026-06-16 16:33:41 +0530

    Added dist folder to track

--- 08-js-action/actions/verify-version/index.js
+++ 08-js-action/actions/verify-version/index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 try {
-  const targetPath = path.join(process.cwd(), "package.json");
+  const targetPath = path.join(__dirname, "..", "..", "..", "package.json");
 
   const packageJson = JSON.parse(fs.readFileSync(targetPath, "utf-8"));
   const version = packageJson.version;
